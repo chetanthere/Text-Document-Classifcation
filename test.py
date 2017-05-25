cd64b65fa8b5bc299eaf58efe4c4244405291dd2
Chetan There <chetan.there@mavs.uta.edu>
2017-05-25 16:44:32 -0500

    Update test.py

--- test.py
+++ test.py
@@ -27,15 +27,9 @@ classlist = []
 for filename in os.listdir(path):
     classlist.append(filename)
 
-
-print(len(classlist))
-print("Here is class list")
-print(classlist)
-
 classdict = {}
 classlen_dict = {}
 traindoc_dict = {}
-
 
 for i in classlist:
     newpath = path + "/" +str(i)
@@ -66,25 +60,20 @@ for i in classlist:
     tokensd = dict(Counter(tokens))
     classdict[i] = tokensd
 
-
 vocablist = []
 for k,v in classdict.items():
-
     vl = list(v.keys())
-
     vlen = sum(v.values())
     classlen_dict[k] = vlen
     for n in vl:
         vocablist.append(n)
 
 #b. Vocabulary and length
-
 vacabset = set(vocablist)
 vocab = list(vacabset)
 vocab_len = (len(vocab))
 print("vocab_len",vocab_len)
 print("classlen_dict",classlen_dict)
-
 
 #c. Form test documents list
 # Here we will deal with each folder separately to calculate accuracy
@@ -102,7 +91,6 @@ for i in classlist:
     correct_class = 0
 
     #testdoc_class_fin = {}
-
     for filename in os.listdir(newpath):
         if filename not in trainl:
             f = open(os.path.join(newpath, filename), "r")
@@ -116,18 +104,14 @@ for i in classlist:
     tokens = []
 
     for m in listl:
-
         tl = tokenizer.tokenize(m)
         tl = [x for x in tl if x not in stopwordslist]
         for n in tl:
             tokens.append(n)
-
         tokensd = {}
         tokensd = dict(Counter(tokens))
         #this is current doc to test of class i
-
         docprob_dict = {}
-
         for k in classlist:
             docprob = 1
             doc_log_prob = 0
@@ -140,10 +124,9 @@ for i in classlist:
                     wordcount = wordcount_dict[j]
                 except KeyError:
                     wordcount = 0
+                    
                 #addding laplace correction
                 wordcount = wordcount + 1
-
-
                 word_prob = wordcount / wordcount_tot
 
                 #make power of # times it occurs in current doc
@@ -154,10 +137,8 @@ for i in classlist:
                 #docprob = docprob * word_prob
                 doc_log_prob = doc_log_prob + word_log_prob
 
-
             #docprob_dict[k] = docprob
             docprob_dict[k] = doc_log_prob
-
 
         #find max prob and decide class
         class_res = max(docprob_dict, key=lambda p: docprob_dict[p])
@@ -167,10 +148,7 @@ for i in classlist:
         if(class_res == i):
             correct_class = correct_class + 1
 
-
     correct_class_dict[i] = correct_class
-
-
 
 print("correct_class_dict",correct_class_dict)
 
@@ -183,7 +161,6 @@ for i in classlist:
 print("total test docs",tsum)
 acc = (csum / tsum) * 100
 print("acc",acc)
-
 
 enddt = datetime.datetime.now()
 print("dttime elapsed",enddt - startdt)
